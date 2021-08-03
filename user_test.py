ebce770bd7605827ced7adc8e89227a024ee4a8e
gagan <gsgagan85@gmail.com>
2021-08-03 18:20:55 +0300

    Add test case to see if objects initialised properly

--- user_test.py
+++ user_test.py
@@ -14,3 +14,12 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("Gagan", "Chhabra", "gsgagan", "password1234")
+
+    def test_init(self):
+        '''
+        test_init test case to see if object is initialised properly
+        '''
+        self.assertEqual(self.new_user.first_name, "Gagan")
+        self.assertEqual(self.new_user.last_name, "Chhabra")
+        self.assertEqual(self.new_user.user_name, "gsgagan")
+        self.assertEqual(self.new_user.password, "password1234")
